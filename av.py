65fcd89de3376aefeabfbea8930918c746197a42
Alex Feldman <alexfeldman93@gmail.com>
2018-12-20 12:59:55 -0800

    Test Av: Audio needs work

--- av.py
+++ av.py
@@ -1,4 +1,5 @@
 import pyaudio
+import time
 import wave
 from picamera import PiCamera
 
@@ -19,19 +20,25 @@ class Camera(object):
 class Audio(object):
     form_1 = pyaudio.paInt16  # 16-bit resolution
     channels = 1  # 1 channel
-    sample_rate = 44100  # 44.1kHz sampling rate
-    chunk = 4096  # 2^12 samples for buffer
-    record_secs = 3  # seconds to record
-    dev_index = 2  # device index found by p.get_device_info_by_index(ii)
+    chunk = 8192
 
     def __init__(self, path):
         self.path = path
         self.audio = None
         self.stream = None
-        self.frames = None
+        self.counter = 0
+
+    def setup(self):
+        self.audio = pyaudio.PyAudio()
+        for i in range(self.audio.get_device_count()):
+            dev = self.audio.get_device_info_by_index(i)
+            if dev['name'] == 'USB Audio Device':
+                break
+        self.sample_rate = (
+            int(self.audio.get_device_info_by_index(i)['defaultSampleRate'])
+        )
 
     def start(self):
-        self.audio = pyaudio.PyAudio()
         self.stream = self.audio.open(
             format=self.form_1,
             rate=self.sample_rate,
@@ -40,12 +47,15 @@ class Audio(object):
             input=True,
             frames_per_buffer=self.chunk
         )
-        self.frames = []
-        for ii in range(0, int((self.sample_rate / self.chunk) * self.record_secs)):
-            data = self.stream.read(self.chunk, exception_on_overflow=False)
-            self.frames.append(data)
+        self.start_time = time.time()
 
     def stop(self):
+        self.end_time = time.time()
+        record_time = self.end_time - self.start_time
+        frames = [
+            self.stream.read(self.chunk) for _ in
+            range(0, int((self.sample_rate / self.chunk) * record_time))
+        ]
         self.stream.stop_stream()
         self.stream.close()
         self.audio.terminate()
@@ -53,5 +63,5 @@ class Audio(object):
         wavefile.setnchannels(self.channels)
         wavefile.setsampwidth(self.audio.get_sample_size(self.form_1))
         wavefile.setframerate(self.sample_rate)
-        wavefile.writeframes(b''.join(self.frames))
+        wavefile.writeframes(b''.join(frames))
         wavefile.close()
--- test.py
+++ test.py
@@ -1,16 +1,33 @@
 import argparse
 import os
-
-from upload import Uploader
+import time
 
 path = os.path.dirname(os.path.realpath(__file__))
 parser = argparse.ArgumentParser(description='Test features')
 parser.add_argument('-u', '--upload', action='store_true', help='Run uploader')
+parser.add_argument('-c', '--camera', action='store_true', help='Test Camera')
+parser.add_argument('-a', '--audio', action='store_true', help='Test Audio record')
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
     if args.upload:
+        from upload import Uploader
         dummy_flight_id = 1
         u = Uploader(dummy_flight_id)
         u.upload(os.path.join(path, 'BOM.pdf'))
+
+    if args.camera:
+        from av import Camera
+        camera = Camera(os.path.join(path, 'cam1.h264'))
+        camera.start()
+        time.sleep(10)
+        camera.stop()
+
+    if args.audio:
+        from av import Audio
+        audio = Audio(os.path.join(path, 'recording.wav'))
+        audio.setup()
+        audio.start()
+        time.sleep(10)
+        audio.stop()
